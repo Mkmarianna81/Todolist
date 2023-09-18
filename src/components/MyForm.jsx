8105d9d0ca80af52e1b80f703a488f5d64e6f1aa
Marianna Mák <makmaiannka1@gmail.com>
2023-09-18 20:09:43 +0200

    first commit

---
+++ src/components/MyForm.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { TextField } from '@mui/material'
+import AddIcon from '@mui/icons-material/Add';
+import { useState } from 'react';
+import { addTodo } from '../utils';
+
+export const MyForm = () => {
+   const [input,setInput]=useState('')
+  return (
+    <div className='form'>
+      <TextField className='input' id="outlined-basic" 
+      label="new todo..." 
+      variant="outlined" 
+      value={input}
+      onChange={(e)=>setInput(e.target.value)}/>
+      
+      <AddIcon sx={{fontSize:'3rem',color:'green'}}
+      onClick={()=>addTodo(input)}
+      />
+    </div>
+  )
+}
+
+
